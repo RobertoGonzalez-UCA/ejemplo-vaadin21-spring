e138a2f2f825a16a5e2d3728c6fb2c7b9f87b913
start.vaadin.com <no-reply@vaadin.com>
2021-11-10 10:02:17 +0000

    Generated project

---
+++ src/main/java/es/uca/iw/data/service/SamplePersonRepository.java
@@ -0,0 +1,10 @@
+package es.uca.iw.data.service;
+
+import es.uca.iw.data.entity.SamplePerson;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, Integer> {
+
+}
